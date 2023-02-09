39173bdaad56b605c276b5acffee478d22984098
Javen Wilson <javenb022@gmail.com>
2023-02-08 20:50:37 -0500

    Add calling to each array

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,8 @@ favorite_colors = ["purple", "blue", "white", "orange"]
 family_age = [22, 21, 1, 0]
 spent_at_walmart = [140.32, 181.35, 155.90, 147.33]
 booleans = [true, false, true, false, false]
+
+puts favorite_colors.pop 
+puts family_age.pop 
+puts spent_at_walmart.pop 
+puts booleans.pop 
