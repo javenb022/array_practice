a26f2acbd6a544e70b686f7e36c137c6e83e6b68
Javen Wilson <javenb022@gmail.com>
2023-02-08 20:48:33 -0500

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+favorite_colors = ["purple", "blue", "white", "orange"]
+family_age = [22, 21, 1, 0]
+spent_at_walmart = [140.32, 181.35, 155.90, 147.33]
+booleans = [true, false, true, false, false]
